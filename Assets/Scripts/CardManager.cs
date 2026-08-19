1763614cc19d03b6b88a5b1c15fe58ae3fd4dcf7
Radon86 <aya10tomoi@gmail.com>
2026-08-19 17:26:29 +0900

    カードマネージャーの準備

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    public GameObject card;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
