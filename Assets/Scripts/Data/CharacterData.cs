f8214a87da9447b05687c03da4ea0093433bc541
Radon86 <aya10tomoi@gmail.com>
2026-08-31 04:21:14 +0900

    キャラクター用のデータ構造の作成

---
+++ Assets/Scripts/Data/CharacterData.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "Scriptable Objects/CharacterData")]
+public class CharacterData : ScriptableObject
+{
+    public List<Character> characters;
+}
+
+[System.Serializable]
+public struct Character
+{
+    public int character_id;
+    public string character_name;
+    public int character_hp;
+    public int character_atk;
+}
